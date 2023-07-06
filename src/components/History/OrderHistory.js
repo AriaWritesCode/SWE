d3d941f35c27a498a4af1cdce2baa5c4668ba8dc
Aria <122402951+AriaWritesCode@users.noreply.github.com>
2023-07-06 16:18:39 -0400

    Add files via upload

---
+++ src/components/History/OrderHistory.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function OrderHistory() {
+  return (
+    <div>
+        {/* This is a temporary blank page for Order History */}
+    </div>
+  );
+}
+
+export default OrderHistory;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';  
+import { Link, useNavigate } from 'react-router-dom';  
+import { UserContext } from '../../App';  
+import './NavBar.css';  
+
+// Functional component for the navigation bar
+function NavBar() {
+    // Use the useNavigate hook for redirecting users
+    const navigate = useNavigate();
+
+    // Access the global state using useContext hook
+    const { isLoggedIn, signOut } = useContext(UserContext);
+
+    // Handler for signing out
+    const handleSignOut = () => {
+        signOut();  // Clear the global state
+        navigate('/');  // Redirect to home page
+    };
+
+    // Return the JSX for the navbar
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><Link to="/" className="nav-link">Home</Link></li>  
+                <li><Link to="/checkout" className="nav-link">Checkout</Link></li>  
+
+                {/* If the user is not logged in, show login and register links */}
+                {!isLoggedIn && (
+                    <>
+                        <li><Link to="/login" className="nav-link">Login</Link></li>  
+                        <li><Link to="/register" className="nav-link">Register</Link></li>  
+                    </>
+                )}
+
+                {/* If the user is logged in, show links for authenticated users */}
+                {isLoggedIn && (
+                    <>
+                        <li><Link to="/orderHistory" className="nav-link">Order History</Link></li>  
+                        <li><Link to="/return" className="nav-link">Return</Link></li>  
+                        <li><Link to="/editProfile" className="nav-link">Edit Profile</Link></li>  
+                        <li><Link to="/" onClick={handleSignOut} className="nav-link">Sign Out</Link></li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
