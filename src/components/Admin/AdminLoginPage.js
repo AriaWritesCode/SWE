275bfa125e41f8b7c4a610b38bbac6cbe9433946
Aria <122402951+AriaWritesCode@users.noreply.github.com>
2023-07-08 20:15:11 -0400

    Add files via upload

---
+++ src/components/Admin/AdminLoginPage.js
@@ -0,0 +1,67 @@
+import React, { useContext, useState } from 'react'; 
+import { UserContext } from '../../App'; 
+import './AdminLoginPage.css'; 
+
+// Login function component
+function AdminLoginPage() {
+  // Extract setIsAminLoggedIn from UserContext
+  const { setIsAdminLoggedIn } = useContext(UserContext);
+  // Initialize state for username and password with useState hook
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // Initialize isSubmitted state to keep track of form submission
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // Handler for form submission
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent form from refreshing the page
+
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
+
+    if (username && password) { // If both fields are filled
+      setUsername(''); // Clear the form
+      setPassword('');
+
+      // Set the submission and login to true
+      setIsSubmitted(true);
+      setIsAdminLoggedIn(true);
+    } 
+  };
+
+  // If the form is submitted successfully, display a message
+  if (isSubmitted) {
+    return <h2>Login Complete</h2>;
+  }
+
+  // Return Login JSX
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <label>
+        Admin Username*
+        <input
+          type="text"
+          placeholder="Required Field"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} 
+          required
+        />
+      </label>
+
+      <label>
+        Password*
+        <input
+          type="password"
+          placeholder="Required Field"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+          required
+        />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default AdminLoginPage;
