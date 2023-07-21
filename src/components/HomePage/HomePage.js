6781ae86ad0c35e308b4e2b788bb472073c2f2d6
Aria <122402951+AriaWritesCode@users.noreply.github.com>
2023-07-21 14:09:44 -0400

    Update HomePage.js

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,48 +3,35 @@ import { UserContext } from '../../App';
 import './HomePage.css'; 
 import { useNavigate } from 'react-router-dom';
 
-
-
-// HomePage function component
 function HomePage() {
-    // Declare state variables using useState
-    const [input, setInput] = useState(''); // For search input field
-    const [searchTerm, setSearchTerm] = useState(''); // For storing the search term
-    const [bookDetails] = useState(''); // To store and display book details
-    const [cartAlert, setCartAlert] = useState(''); // To show alert when item is added to cart
+    const [input, setInput] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [cartAlert, setCartAlert] = useState('');
 
-    // Extract values from UserContext
-    const { isLoggedIn, cartItems, setCartItems, featuredBooks, topSellerBooks  } = useContext(UserContext);
+    const { isLoggedIn, cartItems, setCartItems, featuredBooks, topSellerBooks } = useContext(UserContext);
     
-    // Use useHistory hook here
     const navigate = useNavigate();
 
-    // Function to render book component
     function renderBook(book) {
-        const { id, imageUrl, title, description, price, author } = book; // Extract properties from book object
+        const { bookID, imageUrl, title, price } = book;
 
         const showDetails = () => {
-            // Redirect to the book details page
-            navigate(`/book/${id}`); // We are now using id instead of title
+            navigate(`/book/${bookID}`);
         };
 
-
-        // Function to add a book to cart
         const addToCart = () => {
             if (!isLoggedIn) {
                 alert('Please login first');
                 return;
             }
-            setCartItems([...cartItems, book]); // Add book to cartItems array
-            setCartAlert(`${book.title} has been added to the cart!`); // Show the alert
+            setCartItems([...cartItems, book]);
+            setCartAlert(`${book.title} has been added to the cart!`);
 
-            // Remove the alert after 3 seconds
             setTimeout(() => {
                 setCartAlert('');
             }, 3000);
         };
 
-        // Return JSX for individual book
         return (
             <div className="book">
                 <img src={imageUrl} alt="Book" className="bookImage" />
@@ -52,27 +39,22 @@ function HomePage() {
                 <p>Price: ${price}</p>
                 <button onClick={showDetails} className="btn-details">View Details</button>
                 <button onClick={addToCart} className="btn-addtocart">Add to Cart</button>
-                {bookDetails === description && <p>{description}</p>}
             </div>
         );
     }
 
-        const filteredFeaturedBooks = featuredBooks.filter(book => 
+    const filteredFeaturedBooks = featuredBooks.filter(book => 
         book.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
-        book.author.toLowerCase().includes(searchTerm.toLowerCase()) || 
-        book.isbn.toString().includes(searchTerm)
+        book.author.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const filteredTopSellerBooks = topSellerBooks.filter(book => 
         book.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
-        book.author.toLowerCase().includes(searchTerm.toLowerCase()) || 
-        book.isbn.toString().includes(searchTerm)
+        book.author.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    // Handle search function
     const handleSearch = () => setSearchTerm(input);
 
-    // Return HomePage JSX
     return (
         <div className="homePage">
             <input
